e95013fe6f86f38b351f762c449d12b37ac14514
tossi <mdasiftak21@gmail.com>
2023-06-23 00:04:07 +0530

    some

--- clients/api.ts
+++ clients/api.ts
@@ -1,7 +1,7 @@
 import { GraphQLClient } from 'graphql-request';
 const isClient = typeof window !== 'undefined';
 
-export const graphqlClient = new GraphQLClient('https://d2lz915os3y7pb.cloudfront.net/', {
+export const graphqlClient = new GraphQLClient(process.env.NEXT_PUBLIC_API_URL as string, {
   headers: () => ({
     Authorization: isClient ? `Bearer ${window.localStorage.getItem('__twitter_token')}` : '',
   }),
